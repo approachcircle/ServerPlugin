b5e7da0065befc14dd8ecc37a389320bf17dfac3
approachcircle <osc-mcd@protonmail.com>
2021-12-11 16:13:00 +0000

    rename to more appropriate name

--- src/net/approachcircle/plugin/PlayerDeathListener.java
+++ src/net/approachcircle/plugin/PlayerDeathListener.java
@@ -4,12 +4,14 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
-public class DeathListener implements Listener {
+public class PlayerDeathListener implements Listener {
 	
 	DeathMessages deathMessages = new DeathMessages();
+	VillagerDetector villagerDetector = new VillagerDetector();
 	
 	@EventHandler
 	public void onPlayerDeath(PlayerDeathEvent event) {
 		deathMessages.displayMessage(event);
+		villagerDetector.checkEntityType(event);
 	}
 }
